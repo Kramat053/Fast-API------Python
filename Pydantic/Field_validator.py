5db565c0568621ed37b18ace1980d3676285ca8f
Kramat053 <karamat2053@gmail.com>
2026-06-01 16:57:07 +0500

    a

---
+++ Pydantic/Field_validator.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel,EmailStr,AnyUrl,Field,field_validator
+from typing import List,Dict,Optional,Annotated
+
+class Patient(BaseModel):
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str,str]
+    
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domains = ['abc.com','aaa.com']
+        domain_name = value.split('@')[-1]
+        
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
+        return value
+    
+    @field_validator('name', mode = 'after' )
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+        
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')      
+    
+patient_info = {'name':'Ali','email':'abc@abc.com','linkedin_url':'http://linkedin.com/123','age':'30','weight':75.2,'married':'True','allergies':['pollen','dust'],'contact_details':{'phone':'5752652'}} 
+patient1 = Patient(**patient_info)    
+update_patient_data(patient1)       
